7f1bf5e50d143ede1c92f69022ecc7701203dfad
thamizht670 <41769017+thamizht670@users.noreply.github.com>
2019-08-13 10:04:48 +0530

    Update positivenegative.c

--- positivenegative.c
+++ positivenegative.c
@@ -3,8 +3,10 @@ void main()
 {
  int i;
  scanf("%d",&i);
- if(i>0)
- printf("positive");
+ if(i==0)
+  printf("Zero");
+ else if(i>0)
+  printf("Positive");
  else 
- printf("negative");
+  printf("Negative");
 }
